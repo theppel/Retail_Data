a8f3b2b6d75b5c4ce262669980456903ee69022d
theppel <t.m.heppel@gmail.com>
2026-09-04 21:33:32 +0100

    4/9/26 Evening

--- Notebook.R
+++ Notebook.R
@@ -4,9 +4,9 @@ rm(list = ls())
 # load libraries
 library(tidyverse)
 library(ggplot2)
-library(lme4)
-library(lmerTest)
 library(RSQLite)
+library(shiny)
+library(RColorBrewer)
 
 # set file name where .db file is
 filename <- "retail_store.db"
@@ -70,9 +70,7 @@ cust |>
 head(ords)
 summary(ords)
 
-paste(ords$OrderDate, ords$OrderTime)
-
-# turn date and time into a sinfle datetime object
+# turn date and time into a single datetime object
 ords |>
   mutate(
     OrderDateTime = paste(OrderDate, OrderTime),
@@ -93,10 +91,82 @@ dets |>
   ) |>
   select(!c(ReturnDate, ReturnTime)) -> dets
 
-# add columns for isdiscounted and profit, and profit per unit
+# add columns for isdiscounted,  profit, and profit per unit
 dets |>
   mutate(
     IsDiscounted = DiscountRate > 0,
     Profit = UnitPrice - UnitCost,
     ProfitPerUnit = Profit / Quantity
   ) -> dets
+
+## Shiny Objects ###############################################################
+
+## cust
+
+# creating some ggplot templates
+cust |>
+  ggplot(aes(x = fct_infreq(Gender), fill = Gender)) +
+  geom_bar() +
+  scale_fill_brewer(palette = "Set3") +
+  theme_minimal() +
+  xlab("Gender") +
+  ylab("Frequency") +
+  guides(fill = FALSE)
+
+cust |>
+  ggplot(aes(x = fct_relevel(CustomerSegment, "Standard", "Premium", "VIP"), fill = CustomerSegment)) +
+  geom_bar() +
+  scale_fill_brewer(palette = "Set3") +
+  theme_minimal() +
+  xlab("Segment") +
+  ylab("Frequency") +
+  guides(fill = FALSE)
+
+cust |>
+  ggplot(aes(x = SignUpDate)) +
+  geom_freqpoly()
+
+cust |>
+  mutate(
+    SignUpDate = floor_date(SignUpDate, "month")
+  ) |>
+  count(SignUpDate) |>
+  ggplot(aes(x = SignUpDate, y = n, fill = as_factor(year(SignUpDate)))) +
+  geom_col() +
+  scale_x_date(date_labels = "%Y-%b", date_breaks = "3 months") +
+  guides(fill = FALSE) +
+  theme_minimal() +
+  scale_fill_brewer(palette = "Set3") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "Month", y = "Sign Ups", title = "Sign Ups Time Series")
+
+## ords
+
+ords |>
+  mutate(
+    OrderDatetime = floor_date(OrderDatetime, "month")
+  ) |>
+  count(OrderDatetime) |>
+  ggplot(aes(x = OrderDatetime, y = n, fill = as_factor(year(OrderDatetime)))) +
+  geom_col() +
+  scale_x_date(date_labels = "%Y-%b", date_breaks = "3 months") +
+  guides(fill = FALSE) +
+  theme_minimal() +
+  scale_fill_brewer(palette = "Set3") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "Month", y = "Orders", title = "Orders Time Series")
+
+ords |>
+  mutate(
+    OrderDatetime = floor_date(OrderDatetime, "month")
+  ) |>
+  count(OrderDatetime) |>
+  ggplot(aes(x = OrderDatetime, y = n)) +
+  geom_line(linewidth = 0.75) +
+  scale_x_date(date_labels = "%Y-%b", date_breaks = "3 months") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "Time", y = "Orders", title = "Orders Time Series") +
+  geom_vline(xintercept = dmy(paste0("1-1-", as.character(c(year(min(ords$OrderDatetime)):year(max(ords$OrderDatetime)))))),
+             linetype = "dashed")
+
